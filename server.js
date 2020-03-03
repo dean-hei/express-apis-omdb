1b11ee2455288a4b088ce7417e27f2fc3b2cb021
deanjheikkila <deanjheikkila@gmail.com>
2020-03-02 19:16:01 -0800

    updated stuff

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
+const axios = require('axios');
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -17,11 +18,35 @@ app.use(require('morgan')('dev'));
 
 // Routes
 app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+  res.render('index');
+});
+
+// GET results page
+app.get('/results', function(req, res) {
+  var qs = {
+    params: {
+      s: req.query.title,
+      apikey: process.env.OMDB_API_KEY
+    }
+  }
+  axios.get('https://www.omdbapi.com', qs)
+    .then(function(response) {
+      var results = response.data;
+      console.log(response.data);
+      res.render('results', {returnData: response.data, query: qs.params.s});
+    }).catch(err => {
+      console.log(err);
+      res.send("ERROR");
+    }).then( () => console.log("we made it boys"));
+});
+
+// GET single movie
+app.get('/movies/:id', (req, res) => {
+  res.render('details');
 });
 
 // The app.listen function returns a server handle
-var server = app.listen(process.env.PORT || 3000);
+var server = app.listen(process.env.PORT || 3000, () => {console.log("🎻LISTENING🎻")});
 
 // We can export this server to other servers like this
 module.exports = server;
